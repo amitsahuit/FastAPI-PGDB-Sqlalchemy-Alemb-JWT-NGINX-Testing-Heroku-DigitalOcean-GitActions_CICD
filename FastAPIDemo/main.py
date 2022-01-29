ed5da03c5783fc6768de39938d01bc946a5aece1
Sahu <amit.sahu2@dxc.com>
2022-01-29 06:02:11 -0500

    changing main message

--- FastAPIDemo/main.py
+++ FastAPIDemo/main.py
@@ -43,7 +43,7 @@ app.include_router(FileOperation.router)
 #-----------------------------------------------Testing purpose-----------------------------------------------
 @app.get("/")
 async def root():
-    return {"message": "Hello World lets try Heroku!!!!!!!"}
+    return {"message": "Hello World lets try Digital Ocean & Heroku..!!!!!!!"}
 
 # @app.get("/SQlAlchemyDemo")
 # async def SQlAlchemyDemo(db: Session = Depends(get_db)):
